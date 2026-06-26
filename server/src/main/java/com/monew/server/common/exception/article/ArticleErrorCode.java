8331bbb49eca6b8d648a8d580cba6a61846c0861
lht0415-tech <lth0415@gmail.com>
2026-06-26 18:55:43 +0900

    chore: restore develop files

---
+++ server/src/main/java/com/monew/server/common/exception/article/ArticleErrorCode.java
@@ -0,0 +1,16 @@
+package com.monew.server.common.exception.article;
+
+import com.monew.server.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ArticleErrorCode implements ErrorCode {
+
+    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "뉴스 기사 정보를 찾을 수 없습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
