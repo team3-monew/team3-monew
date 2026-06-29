c7ae5bbf434ae00736c690042c356da0d5f2cde5
nocturne9095 <nocturne9095@gmail.com>
2026-06-29 10:51:14 +0900

    #10 feat : dto 추가(commentRequest)

---
+++ server/src/main/java/com/monew/server/comment/dto/CommentRequest.java
@@ -0,0 +1,10 @@
+package com.monew.server.comment.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CommentRequest(
+    @NotBlank(message = "댓글 내용은 공백일 수 없습니다.")
+    @Size(max = 500, message = "댓글은 최대 500자까지 작성할 수 있습니다.")
+    String content
+) {}
